c3e45c087f0ba892d67bf00489118c069bd14a64
andy <andy@arin.net>
2018-10-27 10:37:08 -0700

    changes to use rx

--- src/main/kotlin/com/swanpipe/verticles/Main.kt
+++ src/main/kotlin/com/swanpipe/verticles/Main.kt
@@ -1,13 +1,20 @@
 // Copyright (C) 2018 Andrew Newton
 package com.swanpipe.verticles
 
-import com.swanpipe.futures.PgInitVerify
-import com.swanpipe.futures.VersionInit
 import com.swanpipe.utils.Db
+import com.swanpipe.utils.Db.dbConfig
+import com.swanpipe.utils.Db.table
+import com.swanpipe.utils.Version
+import io.reactiverse.pgclient.PgClient
+import io.reactiverse.pgclient.PgPoolOptions
+import io.reactivex.Completable
+import io.reactivex.CompletableObserver
 import io.vertx.core.*
+import io.vertx.core.json.JsonObject
 import io.vertx.kotlin.core.DeploymentOptions
 import mu.KLogging
-
+import java.time.OffsetDateTime
+import java.time.format.DateTimeParseException
 
 
 /**
@@ -21,45 +28,21 @@ class Main : AbstractVerticle() {
 
         logger.info( "configuration: ${config()}")
 
-        /**
-         * Stage1 would be things that initialize resources, or perhaps verify resources are available
-         * for a fail-fast start
-         */
-        val stage1 = listOf(
-                VersionInit().logVersion( vertx ),
-                PgInitVerify().execute( vertx, config() )
+        Completable.complete().concatWith {
+            logVersion( it, vertx )
+        }.concatWith {
+            dbInit( it, vertx, config() )
+        }.concatWith {
+            dbVersions( it )
+        } .subscribe(
+                {
+                    logger.info( "SwanPipe startup sequence complete" )
+                    startFuture.complete()
+                },
+                {
+                    startFuture.fail( it )
+                }
         )
-
-        /**
-         * Stage2 are the verticles that are business logice etc...
-         */
-        val stage2 = emptyList<Verticle>()
-
-        /**
-         * Stage3 would bring up the endpoints and things that start "listening" for requests.
-         */
-        val stage3 = emptyList<Verticle>()
-
-        CompositeFuture.all(
-                stage1
-        ).compose { _ ->
-            CompositeFuture.all(
-                    stage2.map{ deployVerticle( it ) }
-            )
-        }.compose { _ ->
-            CompositeFuture.all(
-                    stage3.map{ deployVerticle( it ) }
-            )
-        }.setHandler{ ar ->
-            if( ar.succeeded() ) {
-                logger.info { "Startup Sequence Complete" }
-                startFuture.complete()
-            }
-            else {
-                startFuture.fail( ar.cause() )
-            }
-        }
-
     }
 
     override fun stop(stopFuture: Future<Void>?) {
@@ -90,6 +73,92 @@ class Main : AbstractVerticle() {
         }
         else {
             logger.error( "Deployment of ${result.result()} failed", result.cause() )
+        }
+    }
+
+    fun logVersion(observer: CompletableObserver, vertx: Vertx) {
+        vertx.fileSystem().readFile("version.json") { result ->
+            if (result.succeeded()) {
+                val json = result.result().toJsonObject()
+                val version: String? = json.getString("version")
+                val buildDate: String? = json.getString("buildDate")
+                if (version == null || buildDate == null) {
+                    logger.error("unable to get version information")
+                    observer.onError(RuntimeException("unable to get version information"))
+                } else {
+                    Version.version = version
+                    try {
+                        Version.buildDate = OffsetDateTime.parse(buildDate)
+                        logger.info("Starting version=${Version.version} buildDate=${Version.buildDate}")
+                        observer.onComplete()
+                    } catch (e: DateTimeParseException) {
+                        logger.error(e.message)
+                        observer.onError(e)
+                    }
+                }
+            } else {
+                logger.error("unable to read version file")
+                observer.onError(RuntimeException("unable to read version file"))
+            }
+        }
+    }
+
+    fun dbInit(observer: CompletableObserver, vertx: Vertx, config: JsonObject) {
+        Db.config = config
+        if (Db.isConfigured()) {
+
+            val options = PgPoolOptions(dbConfig)
+
+            // Create the client pool
+            val client = PgClient.pool(vertx, options)
+
+            // A simple query
+            Db.configuredFlywayVerstion = dbConfig.getString("flywayVersion")
+            val sql = "select version, installed_on from ${table("flyway_schema_history")} order by version desc"
+            client.query(sql) { ar ->
+                if (ar.succeeded()) {
+                    val result = ar.result()
+                    logger.trace("Got ${result.size()} rows ")
+                    var versionMatch = false
+                    result.forEachIndexed { index, row ->
+                        if (index == 0) {
+                            Db.flywayVersion = row.getString(0)
+                            Db.installedOn = row.getLocalDateTime(1)
+                        }
+                        if (row.getString(0).equals(Db.configuredFlywayVerstion)) {
+                            versionMatch = true
+                        }
+                    }
+                    if (!versionMatch) {
+                        logger.error("flyway version does not match")
+                        observer.onError(RuntimeException("flyway version does not match"))
+                    } else {
+                        logger.info("Database flyway version ${Db.configuredFlywayVerstion} confirmed.")
+                        logger.info("Database is at version ${Db.flywayVersion} install on ${Db.installedOn}")
+                        Db.pgPool = client
+                        observer.onComplete()
+                    }
+                } else {
+                    logger.error("Flyway inspection failure: sql=${sql} message=${ar.cause().message}")
+                    observer.onError(ar.cause())
+                }
+            }
+        } else {
+            logger.error { "database not configured: dbConfig = ${dbConfig}" }
+            observer.onError(RuntimeException("database does not appear to be configured"))
+        }
+    }
+
+    fun dbVersions(observer: CompletableObserver) {
+        Db.pgPool.query("select version, installed_on from ${table("flyway_schema_history")}") { ar ->
+            if (ar.succeeded()) {
+                ar.result().forEach { row ->
+                    logger.info("version = ${row.getString(0)} installed on = ${row.getLocalDateTime(1)}")
+                }
+                observer.onComplete()
+            } else {
+                observer.onError(ar.cause())
+            }
         }
     }
 }
