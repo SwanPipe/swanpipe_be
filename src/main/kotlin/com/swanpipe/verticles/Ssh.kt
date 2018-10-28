0edc3a7bc9ad77ccaf2d2e93e22663d2807984b8
andy <andy@arin.net>
2018-10-28 11:39:56 -0700

    first attempt at an ssh command

--- src/main/kotlin/com/swanpipe/verticles/Ssh.kt
+++ src/main/kotlin/com/swanpipe/verticles/Ssh.kt
@@ -1,6 +1,10 @@
 // Copyright (C) 2018 Andrew Newton
 package com.swanpipe.verticles
 
+import com.swanpipe.utils.Db
+import com.swanpipe.utils.Db.table
+import io.reactiverse.reactivex.pgclient.PgClient
+import io.reactivex.Single
 import io.vertx.core.AbstractVerticle
 import io.vertx.ext.shell.ShellService
 import io.vertx.ext.shell.ShellServiceOptionsConverter
@@ -13,9 +17,17 @@ import io.vertx.kotlin.ext.shell.ShellServiceOptions
 class Ssh : AbstractVerticle() {
     override fun start() {
 
-        var helloWorld = CommandBuilder.command("hello-world").processHandler { process ->
-            process.write("hello world\n")
-            process.end()
+        var createAccount = CommandBuilder.command("create-account").processHandler { process ->
+            createAccount().subscribe(
+                    { id ->
+                        process.write( "Account created with ID ${id}\n")
+                        process.end()
+                    },
+                    {
+                        process.write(( "Error creating account: ${it.message}\n"))
+                        process.end( 1 )
+                    }
+            )
         }.build(vertx)
 
         val sshConfig = json {
@@ -36,11 +48,18 @@ class Ssh : AbstractVerticle() {
         val options = ShellServiceOptions()
         ShellServiceOptionsConverter.fromJson( sshConfig, options )
         val service = ShellService.create(vertx, options )
-        CommandRegistry.getShared(vertx).registerCommand(helloWorld)
+        CommandRegistry.getShared(vertx).registerCommand(createAccount)
         service.start { ar ->
             if (!ar.succeeded()) {
                 ar.cause().printStackTrace()
             }
         }
+    }
+
+    fun createAccount() : Single<Int> {
+        return PgClient( Db.pgPool ).rxQuery( "insert into ${table("account")} ( id ) values (default) returning id")
+                .map { pgRowSet ->
+                    pgRowSet.iterator().next().getInteger( "id" )
+                }
     }
 }
