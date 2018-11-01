bd507b8eb8f3b696011ba71eea23871faaca6a10
andy <andy@hxr.us>
2018-11-01 10:39:45 -0400

    changing the datamodel

--- src/main/resources/db/migration/V201810250844__initial.sql
+++ src/main/resources/db/migration/V201810250844__initial.sql
@@ -14,26 +14,12 @@
  * limitations under the License.
  */
 
-
 /*
- Accounts simply tie together logins and personas.
- Each account can have multiple logins (a team of people
- under one account) and each account can have multiple
- personas (multiple personalities).
- */
-create table account (
-  id serial not null primary key,               -- account id
-  enabled boolean not null default true,        -- is the account enabled
-  created timestamptz not null default now()    -- when was it created
-);
-
-/*
- Logins allow a team of people to use one account.
+ Logins allow a team of people to act as one persona.
  Each login id must be unique system wide.
  */
 create table login (
   id text not null primary key,                          -- otherwise known as a login name or user name
-  account_id integer not null references account( id ),  -- under which acocunt
   password text not null,
   enabled boolean not null default true,
   created timestamptz not null default now(),
@@ -48,7 +34,16 @@ create table login (
  */
 create table persona (
   id text not null primary key,                         -- the fediverse handle, such as @foo
-  account_id integer not null references account( id ), -- which account does it belong to
   display_name text,                                    -- the display name
   created timestamptz not null default now()            -- when the persona was created
+);
+
+/*
+ This table ties logins and personas together.
+ */
+create table login_persona_link (
+  login_id text not null references login( id ),
+  persona_id text not null references persona( id ),
+  owner boolean not null default true,               -- determines that the login id has persona ownership privledges
+  primary key( login_id, persona_id )
 );
