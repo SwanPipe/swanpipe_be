83d87d4252dec76d320db253ddd1614910825789
andy <andy@arin.net>
2018-11-19 13:09:47 -0800

    beginnings of JWT

--- src/main/kotlin/com/swanpipe/routers/OpenApi3Router.kt
+++ src/main/kotlin/com/swanpipe/routers/OpenApi3Router.kt
@@ -17,14 +17,31 @@
 package com.swanpipe.routers
 
 import com.swanpipe.utils.appLogger
+import io.vertx.core.json.JsonObject
+import io.vertx.ext.auth.jwt.JWTAuthOptions
 import io.vertx.kotlin.core.json.json
 import io.vertx.kotlin.core.json.obj
 import io.vertx.kotlin.ext.web.api.contract.RouterFactoryOptions
 import io.vertx.reactivex.core.Vertx
+import io.vertx.reactivex.ext.auth.jwt.JWTAuth
 import io.vertx.reactivex.ext.web.Router
 import io.vertx.reactivex.ext.web.api.contract.openapi3.OpenAPI3RouterFactory
+import java.time.OffsetDateTime
+
 
 fun openApi3Router( vertx: Vertx, parent: Router ) {
+
+    val jwtAuthConfig = json {
+        obj(
+            "keyStore" to
+                    obj(
+                        "type" to "jceks",
+                        "path" to "jwt.jceks",
+                        "password" to "secret"
+                    )
+        )
+    }
+    val jwt = JWTAuth.create( vertx, JWTAuthOptions( jwtAuthConfig ) )
 
     OpenAPI3RouterFactory.rxCreate( vertx, "spv1.yaml" )
         .subscribe { rf ->
@@ -34,7 +51,11 @@ fun openApi3Router( vertx: Vertx, parent: Router ) {
                 rc.response()
                     .end(
                         json { obj(
-                            "token" to "foo"
+                            "token" to jwt.generateToken( JsonObject()
+                                .put( "sub", "bob" )
+                                .put( "iss", "swanpipe")
+                                .put( "exp", OffsetDateTime.now().plusMonths( 1 ).toEpochSecond() )
+                            )
                         ) }.encodePrettily()
                     )
             }
