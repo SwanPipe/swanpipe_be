0e03cae77c75ae0c9dfc172884d3b20a4dbc247d
andy <andy@arin.net>
2018-11-21 06:11:28 -0800

    optimized imports

--- src/main/kotlin/com/swanpipe/routers/OpenApi3Router.kt
+++ src/main/kotlin/com/swanpipe/routers/OpenApi3Router.kt
@@ -20,19 +20,16 @@ import com.swanpipe.actions.LoginActions.checkLogin
 import com.swanpipe.daos.LoginDao.getLogin
 import com.swanpipe.utils.AUTHORIZATION_HEADER
 import com.swanpipe.utils.appLogger
-import io.vertx.core.http.HttpMethod
 import io.vertx.core.json.JsonObject
 import io.vertx.ext.auth.jwt.JWTAuthOptions
 import io.vertx.kotlin.core.json.json
 import io.vertx.kotlin.core.json.obj
 import io.vertx.kotlin.ext.web.api.contract.RouterFactoryOptions
 import io.vertx.reactivex.core.Vertx
-import io.vertx.reactivex.ext.auth.User
 import io.vertx.reactivex.ext.auth.jwt.JWTAuth
 import io.vertx.reactivex.ext.web.Router
 import io.vertx.reactivex.ext.web.RoutingContext
 import io.vertx.reactivex.ext.web.api.contract.openapi3.OpenAPI3RouterFactory
-import io.vertx.reactivex.ext.web.handler.JWTAuthHandler
 import java.time.OffsetDateTime
 
 
