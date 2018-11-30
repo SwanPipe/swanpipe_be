acd282e965b7ba6b6c013a575162f48b1aac7200
andy <andy@arin.net>
2018-11-30 07:20:01 -0800

    first pass at working startup accounts

--- src/main/kotlin/com/swanpipe/actions/StartupAccountsActions.kt
+++ src/main/kotlin/com/swanpipe/actions/StartupAccountsActions.kt
@@ -16,7 +16,9 @@
 package com.swanpipe.actions
 
 import com.swanpipe.actions.LoginActions.ID
+import com.swanpipe.daos.ConfigDao
 import com.swanpipe.daos.LoginDao
+import com.swanpipe.utils.appLogger
 import io.reactivex.Observable
 import io.reactivex.Single
 import io.vertx.core.json.JsonArray
@@ -25,64 +27,70 @@ import io.vertx.core.json.JsonObject
 const val STARTUP_ACCOUNTS = "startupAccounts"
 const val ACTOR_LOGINS = "actorLogins"
 
-fun createStartupActorLogins( config: JsonObject) : Single<String> {
-    return Single.create { emitter ->
-        if( config.getJsonObject( STARTUP_ACCOUNTS ) != null &&
-            config.getJsonObject(STARTUP_ACCOUNTS).getJsonArray(ACTOR_LOGINS) != null ) {
-            val actorLogins = config.getJsonObject( STARTUP_ACCOUNTS ).getJsonArray( ACTOR_LOGINS )
-            existingLogins( actorLogins )
+fun doStartupAccounts( config: JsonObject ) : Single<Boolean> {
+    val saConfig = config.getJsonObject( STARTUP_ACCOUNTS )
+    if( saConfig != null ) {
+        return Single.create{ emitter ->
+            ConfigDao.getConfig( STARTUP_ACCOUNTS )
                 .subscribe(
-                    { loginIds ->
-                        Observable.fromIterable( actorLogins )
-                            .filter { i ->
-                                if( i is JsonObject ) {
-                                    !loginIds.contains(i.getString( ID ) )
+                    { config ->
+                        if( config.data.getBoolean("create" )) {
+                            createStartupActorLogins( saConfig )
+                                .flatMap {
+                                    ConfigDao.setConfig( STARTUP_ACCOUNTS, JsonObject().put( "create", false ) )
                                 }
-                                else {
-                                    throw java.lang.RuntimeException( "it is not JSON object. is ${i::class.java}")
-                                }
-                            }
-                            .flatMap { login ->
-                                ActorLoginActions.createActorLogin( login as JsonObject).toObservable()
-                            }
-                            .collect( { ArrayList<Triple<String,String,Boolean>>() }, { a,i -> a.add( i )} )
-                            .subscribe(
-                                {
-                                    emitter.onSuccess( "${it.size} actor logins created" )
-                                },
-                                {
-                                    emitter.onError( it )
-                                }
-                            )
+                                .subscribe(
+                                    {
+                                        emitter.onSuccess( true )
+                                    },
+                                    {
+                                        emitter.onError( it )
+                                    }
+                                )
+                        }
+                        else {
+                            appLogger.info { "Startup accounts create configuration set to false" }
+                            emitter.onSuccess(false )
+                        }
                     },
                     {
-                        throw( it )
+                        emitter.onError( it )
+                    },
+                    {
+                        appLogger.info { "No startup accounts configuration." }
+                        emitter.onSuccess(false )
+                    }
+                )
+        }
+    }
+    else {
+        return Single.just( false )
+    }
+}
+
+fun createStartupActorLogins( saConfig: JsonObject) : Single<Boolean> {
+    return Single.create { emitter ->
+        if( saConfig.getJsonArray(ACTOR_LOGINS) != null ) {
+            val actorLogins = saConfig.getJsonArray( ACTOR_LOGINS )
+            Observable.fromIterable( actorLogins )
+                .subscribe(
+                    { actorLogin ->
+                        ActorLoginActions.createActorLogin( actorLogin as JsonObject )
+                            .subscribe { result : Triple<String,String,Boolean> ->
+                                appLogger.info { "Login ${result.first} with actor ${result.second} created" }
+                            }
+                    },
+                    {
+                        emitter.onError( it )
+                    },
+                    {
+                        emitter.onSuccess( true )
                     }
                 )
         }
         else {
-            emitter.onSuccess( "no actor logins configured" )
+            emitter.onSuccess( false )
         }
     }
 }
 
-fun existingLogins( logins : JsonArray) : Single<ArrayList<String>> {
-    return Observable.fromIterable( logins )
-        .flatMap { i ->
-            if( i is JsonObject ) {
-                LoginDao.getLogin( i.getString( ID ) ).toObservable()
-            }
-            else {
-                throw java.lang.RuntimeException( "it is not JSON object. is ${i::class.java}")
-            }
-
-        }
-        .collect(
-            {
-                ArrayList<String>()
-            },
-            { list, login ->
-                list.add( login.id )
-            }
-        )
-}
---
+++ src/test/kotlin/com/swanpipe/actions/StartupAccountsActionsTest.kt
@@ -0,0 +1,140 @@
+/*
+ * Copyright (c) 2018. Andrew Newton
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.swanpipe.actions
+
+import com.swanpipe.InitPg
+import com.swanpipe.daos.ActorDao
+import com.swanpipe.daos.ConfigDao
+import com.swanpipe.daos.LoginDao
+import com.swanpipe.utils.Db
+import com.swanpipe.utils.ValidationException
+import io.vertx.core.Vertx
+import io.vertx.core.json.JsonObject
+import io.vertx.junit5.VertxExtension
+import io.vertx.junit5.VertxTestContext
+import io.vertx.kotlin.core.json.array
+import io.vertx.kotlin.core.json.json
+import io.vertx.kotlin.core.json.obj
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.*
+import org.junit.jupiter.api.extension.ExtendWith
+import org.mindrot.jbcrypt.BCrypt
+
+@DisplayName("Test of login daos")
+@ExtendWith(VertxExtension::class)
+object StartupAccountsActionsTest {
+
+    @DisplayName("Prepare the database")
+    @BeforeAll
+    @JvmStatic
+    fun prepare(testContext: VertxTestContext) {
+        InitPg.startPg()
+        testContext.completeNow()
+    }
+
+    @DisplayName("close database")
+    @AfterAll
+    @JvmStatic
+    fun cleanUp(testContext: VertxTestContext) {
+        Db.pgPool.close()
+        testContext.completeNow()
+    }
+
+    @DisplayName("Setup data")
+    @BeforeEach
+    fun prepareEach(testContext: VertxTestContext) {
+        InitPg.clean().migrate()
+        testContext.completeNow()
+    }
+
+    @DisplayName("Test create login action")
+    @Test
+    fun testCreateLogin(vertx: Vertx, testContext: VertxTestContext) {
+
+        //by default, the startupAccounts is set to true in flyway
+
+        // our test data
+        val sa = JsonObject(
+            """
+{
+  "startupAccounts" : {
+    "actorLogins" : [
+      {
+        "id" : "admin",
+        "password" : "secret",
+        "pun" : "beautifulSwan",
+        "loginData" : {
+          "email" : "foo@example.com",
+          "roles" : [ "admin" ]
+        }
+      },
+      {
+        "id" : "admin2",
+        "password" : "anothersecret",
+        "pun" : "thedude",
+        "loginData" : {
+          "email" : "bar@example.com"
+        }
+      }
+    ],
+    "actors" : [
+      {
+        "pun" : "ambassador"
+      }
+    ],
+    "links" : [
+      {
+        "loginId" : "admin",
+        "pun" : "ambassador",
+        "owner" : true
+      }
+    ]
+  }
+}
+            """.trimIndent()
+        )
+
+        InitPg.pool(vertx)
+        doStartupAccounts( sa )
+            .flatMapMaybe {
+                testContext.verify {
+                    assertThat( it ).isTrue()
+                }
+                LoginDao.getLogin( "admin" )
+            }
+            .flatMap {
+                ActorDao.getActor( "beautifulSwan" )
+            }
+            .flatMap {
+                ConfigDao.getConfig( STARTUP_ACCOUNTS )
+            }
+            .subscribe(
+                { config ->
+                    testContext.verify {
+                        assertThat( config.data.getBoolean( "create" ) ).isFalse()
+                    }
+                    testContext.completeNow()
+                },
+                {
+                    testContext.failNow( it )
+                }
+            )
+    }
+
+
+}
+
